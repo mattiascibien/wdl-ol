4f71ee5c3d87821a63500cc6f8b2d7ac1f8c64d0
Julijan Nikolic <julijannikolic@yahoo.com>
2017-03-19 21:40:47 +0100

    UPDATE: Updated IPlugEffectCairoGraphic because of last API change.

--- IPlugExamples/IPlugEffectCairoGraphics/IPlugEffectCairoGraphics.h
+++ IPlugExamples/IPlugEffectCairoGraphics/IPlugEffectCairoGraphics.h
@@ -24,12 +24,12 @@ class CustomCairoControl : public IControl, ycairo_gui
 {
 public:
 	CustomCairoControl(IPlugBase* pPlug, ycairo_base *ycairo_base, IRECT pR)
-		: IControl(pPlug, pR), ycairo_gui(ycairo_base, pR) {}
+		: IControl(pPlug, pR), ycairo_gui(ycairo_base, this) {}
 
 
 	bool Draw(IGraphics* pGraphics)
 	{
-		ycairo_prepare_draw(mDrawRECT);
+		ycairo_prepare_draw();
 
 		// Custom cairo code -----------------------------------------------------------------------------------------------------------------------------------
 
