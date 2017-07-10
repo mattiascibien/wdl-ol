e851c72703a137c4658b2ae2cec2b38eaeff4f49
Julijan Nikolic <julijannikolic@yahoo.com>
2017-07-10 14:11:07 +0200

    UPDATE: Added drop shadow for paths option. Changed ycairo_graphics_helper to just ycairo_helper.

--- YCAIRO/ycairo.h
+++ YCAIRO/ycairo.h
@@ -55,8 +55,8 @@
 #include FT_FREETYPE_H
 
 #include <string>
+#include <math.h>
 #include "../WDL/IPlug/IControl.h"
-
 
 typedef enum _ycairo_text_w_aligement { YCAIRO_TEXT_W_ALIGN_LEFT, YCAIRO_TEXT_W_ALIGN_RIGHT, YCAIRO_TEXT_W_ALIGN_CENTER } ycairo_text_w_aligement;
 typedef enum _ycairo_text_h_aligement { YCAIRO_TEXT_H_ALIGN_CENTER, YCAIRO_TEXT_H_ALIGN_TOP, YCAIRO_TEXT_H_ALIGN_BOTTOM } ycairo_text_h_aligement;
@@ -136,7 +136,81 @@ private:
 #endif
 };
 
-class ycairo_graphics_helper
+class _ycairo_pixel_average
+{
+protected:
+	// Internal framework functions. --------------------------------------------------------------------------------------------------------------------------------
+	inline void _average_channel(unsigned char * in, unsigned int start_index, unsigned int size, unsigned int delay_stride);
+	inline void _average_pixel(unsigned char * in, unsigned int start_index, unsigned int size, unsigned int delay_stride);
+
+	inline void _average_channel_minus_radius(unsigned char *in, unsigned int size, unsigned int delay_stride, unsigned int max_sum);
+	inline void _average_pixel_minus_radius(unsigned char *in, unsigned int size, unsigned int delay_stride);
+	
+	inline void _average_channel_bit_shift(unsigned char *in, unsigned int size, unsigned int delay_stride, unsigned int max_sum);
+	inline void _average_pixel_bit_shift(unsigned char *in, unsigned int size, unsigned int delay_stride);
+
+	inline void _reset();
+
+	int out = 0, out1 = 0, out2 = 0, out3 = 0, out4 = 0;
+};
+
+class ycairo_blur : public _ycairo_pixel_average
+{
+protected:
+	// Internal framework functions. --------------------------------------------------------------------------------------------------------------------------------
+	void _ycairo_blur_surface_offseted_minus_radius(cairo_surface_t * src_surface, unsigned int radius, unsigned int channels_num = 4);	
+	void _ycairo_blur_surface_channel_offseted_minus_radius(cairo_surface_t * src_surface, unsigned int radius, unsigned int channel, int unsigned channels_num = 4);
+	
+	void _ycairo_blur_surface_offseted(cairo_surface_t * src_surface, unsigned int radius, unsigned int channels_num = 4);
+	void _ycairo_blur_surface_channel_offseted(cairo_surface_t * src_surface, unsigned int radius, unsigned int channel, unsigned int channels_num = 4);
+
+	inline unsigned int _get_bit_shift(unsigned int radius);
+};
+
+class ycairo_drop_shadow : public ycairo_blur
+{
+public:
+
+	// Set shadow properties
+	void ycairo_drop_shadow_set_opacity(cairo_t *cr, double opacity);
+	void ycairo_drop_shadow_set_radius(cairo_t *cr, double radius);
+	void ycairo_drop_shadow_set_distance(cairo_t *cr, double distance);
+	void ycairo_drop_shadow_set_angle(cairo_t *cr, double angle);
+	
+	// Use downsample to improve shadow speed. Ussualy going below 4x (8x with bigger radius) doesn't give you more performance improvement.
+	// If you want to go below 4x use fast shadow instead but you won't get much more performance improvement (~20%).
+	// Using radius of size 2, 4, 8, 16, 32, 64, 128, 256, 512 and 1024 will improve performance by ~(25% / downsample).
+	void ycairo_drop_shadow_fill(cairo_t *cr, double downsample = 4);
+
+	// Fast drop shadow is ~4x faster (with downsample == 1), but it can be used on non moving objects or with small radius with good results only.
+	void ycairo_drop_shadow_fill_fast(cairo_t *cr);
+
+	void ycairo_drop_shadow_stroke(cairo_t *cr, double downsample = 4);
+	void ycairo_drop_shadow_stroke_fast(cairo_t * cr);
+
+private:
+	void _ycairo_draw_drop_shadow_fast(cairo_t *cr, bool stroke);
+	void _ycairo_draw_drop_shadow(cairo_t *cr, bool stroke, double downsample);
+	void _calculate_shadow_offset(int props_index);
+	int _get_props_index(cairo_t *cr);
+
+	struct shadow_properties
+	{
+		cairo_t * cr = NULL;
+
+		double shadow_opacity = 0.5;
+		double shadow_radius = 16;
+		double shadow_distance = 0.0;
+		double shadow_angle = 90;
+
+		double shadow_offset_x = 0;
+		double shadow_offset_y = 0;
+	};
+
+	vector<shadow_properties> props;
+};
+
+class ycairo_helper
 {
 public:
 	void ycairo_rounded_rectangle(cairo_t *cr, double x, double y, double width, double height, double corner);
@@ -149,6 +223,7 @@ public:
 	void ycairo_set_source_rgba_fast(cairo_t *cr, IColor *color);
 
 	void ycairo_reset_clip_to(cairo_t *cr, IRECT rect);
+
 };
 
 class ycairo_text
@@ -206,7 +281,7 @@ private:
 
 };
 
-class ycairo_gui : public ycairo_graphics_helper
+class ycairo_gui : public ycairo_helper
 {
 public:
 	ycairo_gui(ycairo_base *ycairo_base, IControl *pControl);
