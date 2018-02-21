01b92b74ca00ae4e4dc49e81d262d62fda000e6b
Julijan Nikolic <julijannikolic@yahoo.com>
2018-02-21 15:45:32 +0100

    UPDATE: Reverted to freeetype 2.8.1 because 2.9 has some problems with building.

--- YCAIRO/Cairo_Graphics/freetype/src/base/ftbase.c
+++ YCAIRO/Cairo_Graphics/freetype/src/base/ftbase.c
@@ -4,7 +4,7 @@
 /*                                                                         */
 /*    Single object library component (body only).                         */
 /*                                                                         */
-/*  Copyright 1996-2018 by                                                 */
+/*  Copyright 1996-2017 by                                                 */
 /*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
 /*                                                                         */
 /*  This file is part of the FreeType project, and may only be used,       */
@@ -29,7 +29,6 @@
 #include "ftobjs.c"
 #include "ftoutln.c"
 #include "ftpic.c"
-#include "ftpsprop.c"
 #include "ftrfork.c"
 #include "ftsnames.c"
 #include "ftstream.c"
--- YCAIRO/Cairo_Graphics/freetype/src/sfnt/ttcmap.c
+++ YCAIRO/Cairo_Graphics/freetype/src/sfnt/ttcmap.c
@@ -4,7 +4,7 @@
 /*                                                                         */
 /*    TrueType character mapping table (cmap) support (body).              */
 /*                                                                         */
-/*  Copyright 2002-2018 by                                                 */
+/*  Copyright 2002-2017 by                                                 */
 /*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
 /*                                                                         */
 /*  This file is part of the FreeType project, and may only be used,       */
@@ -518,11 +518,7 @@
 
 
         if ( offset == 0 )
-        {
-          if ( charcode == 0x100 )
-            goto Exit; /* this happens only for a malformed cmap */
           goto Next_SubHeader;
-        }
 
         if ( char_lo < start )
         {
@@ -551,19 +547,9 @@
         }
       }
 
-      /* If `charcode' is <= 0xFF, retry with `charcode + 1'.  If        */
-      /* `charcode' is 0x100 after the loop, do nothing since we have    */
-      /* just reached the first sub-header for two-byte character codes. */
-      /*                                                                 */
-      /* For all other cases, we jump to the next sub-header and adjust  */
-      /* `charcode' accordingly.                                         */
+      /* jump to next sub-header, i.e. higher byte value */
     Next_SubHeader:
-      if ( charcode <= 0xFF )
-        charcode++;
-      else if ( charcode == 0x100 )
-        ;
-      else
-        charcode = FT_PAD_FLOOR( charcode, 0x100 ) + 0x100;
+      charcode = FT_PAD_FLOOR( charcode, 256 ) + 256;
     }
 
   Exit:
