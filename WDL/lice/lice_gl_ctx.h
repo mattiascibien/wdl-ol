85969bec46d2359c43009a0a3a7055ba4a5d382e
Mattias Cibien <mattias@mattiascibien.net>
2018-03-02 10:15:59 +0100

    OpenGL 3.3 for lice gl contest

--- WDL/lice/lice_gl_ctx.h
+++ WDL/lice/lice_gl_ctx.h
@@ -3,10 +3,7 @@
 
 #include "lice.h"
 
-#define GLEW_STATIC
-#include "glew/include/gl/glew.h"
-#include "glew/include/gl/wglew.h"
-#include "glew/include/gl/wglext.h"
+#include "glad/include/glad/glad.h"
 
 // GL context functions
 // opening and managing GL context is handled behind the scenes
@@ -17,8 +14,6 @@ bool LICE_GL_IsValid(); // GL context is initialized (will be lazy initialized o
 HWND LICE_GL_GetWindow(); // Get the window that owns the GL context (one per process)
 
 void LICE_GL_CloseCtx();  // Something failed, turn off GL context forever so we don't keep failing
-
-GLUnurbsObj* LICE_GL_GetNurbsObj(int linetol=8);  // linetol = maximum number of straight-line pixels
 
 // facility for associating a glyph with a texture
 GLuint LICE_GL_GetTexFromGlyph(const unsigned char* glyph, int glyph_w, int glyph_h); 
